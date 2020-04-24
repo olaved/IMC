9cdccc0d00ef5eaf0081d88ed8e141e174df03b7
olaved <37856821+olaved@users.noreply.github.com>
2020-04-24 15:41:03 -0300

    commit 2

--- IMC/src/IMC/main.java
+++ IMC/src/IMC/main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class main {
 	
 	
-			
+	
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner (System.in);
 		
+		//		System.out.println("Calculadora de IMC");
 		System.out.println("Calculadora de IMC");
 		System.out.println("Ingresa la altura");
 		String altura = sc.nextLine();
